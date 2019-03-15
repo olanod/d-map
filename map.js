f33d162d378026861763ab8fa7f64b44bcb94e0b
Daniel Olano <danielolanoc@gmail.com>
2019-03-15 11:52:03 +0100

    Change attribute name for coordinates and center (#2)
    * Change attribute name for coordinates and center
    
    Also bump mbxgl version and do some refactoring

--- map.js
+++ map.js
@@ -1,96 +1,103 @@
 /* Copyright © 2018  Daniel Olano | @license GPLv3 */
-const mbxVer = 'v0.51.0'
-const mbxJS = `https://api.tiles.mapbox.com/mapbox-gl-js/${mbxVer}/mapbox-gl-dev.js`
+const mbxVer = 'v0.53.1'
+const mbxJS = `https://api.tiles.mapbox.com/mapbox-gl-js/${mbxVer}/mapbox-gl.js`
 const mbxCSS = `https://api.tiles.mapbox.com/mapbox-gl-js/${mbxVer}/mapbox-gl.css`
-const mbxStyle = document.head.querySelector('meta[name=mapbox-style]') || 'mapbox://styles/mapbox/streets-v9'
-const mbxToken = document.head.querySelector('meta[name=mapbox-token]')
-const mapboxReady = loadMapbox(mbxJS)
+const mbxStyle = 'mapbox://styles/mapbox/streets-v9'
+const mbxToken = document.head.querySelector('meta[name=mapbox-token]').content
+
+;(async () => {
+	await loadMapbox(mbxJS, mbxToken)
+	customElements.define(Map.tag, Map)
+	customElements.define(Marker.tag, Marker)
+})()
 
 export const mapbox = Symbol('mapbox')
 
+/**
+ * Map
+ */
 export class Map extends HTMLElement {
-  static get tag() { return 'do-map' }
+	static get tag() { return 'd-map' }
 
-  constructor() {
-    super()
-    this._center = [0, 0]
-    this._zoom = parseInt(this.getAttribute('zoom')) || 0
+	constructor() {
+		super()
+		this._center = parseCoords(this.getAttribute('c'))
+		this._zoom = +this.getAttribute('z') || 0
 
-    const css = document.createElement('style')
-    css.append(document.createTextNode(`
-    :host {display: block}
-    #map {height: 100%; width: 100%;}
-    `))
+		const css = document.createElement('style')
+		css.append(document.createTextNode(`:host {display: block}
+#map {height: 100%; width: 100%;}`))
 
-    const mcss = document.createElement('link')
-    mcss.rel = 'stylesheet'
-    mcss.href = mbxCSS
+		const mcss = document.createElement('link')
+		mcss.rel = 'stylesheet'
+		mcss.href = mbxCSS
 
-    const map = document.createElement('div')
-    map.id = 'map'
-    Object.defineProperty(this, '$map', { value: map })
+		const map = document.createElement('div')
+		map.id = 'map'
+		Object.defineProperty(this, '$map', { value: map })
 
-    this.attachShadow({ mode: 'open' })
-    this.shadowRoot.append(mcss, css, map)
-  }
+		this.attachShadow({ mode: 'open' })
+		this.shadowRoot.append(mcss, css, map)
+	}
 
-  async connectedCallback() {
-    await mapboxReady
-    this[mapbox] = new mapboxgl.Map({
-      container: this.$map,
-      style: mbxStyle,
-      center: this._center,
-      zoom: this._zoom,
-    })
-  }
+	connectedCallback() {
+		this[mapbox] = new mapboxgl.Map({
+			container: this.$map,
+			style: mbxStyle,
+			center: this._center,
+			zoom: this._zoom,
+		})
+	}
 
-  get zoom() { return this._zoom }
-  set zoom(val) { set(this, 'zoom', val) }
+	set(prop, val) { setProp(this, prop, val) }
 
-  get center() { return this._center }
-  set center(val) { set(this, 'center', val) }
+	get zoom() { return this._zoom }
+	set zoom(val) { this.set('zoom', val) }
+
+	get center() { return this._center }
+	set center(val) { this.set('center', val) }
 }
-customElements.define(Map.tag, Map)
 
+
+/**
+ * Marker
+ */
 export class Marker extends HTMLElement {
-  static get tag() { return 'do-marker' }
+	static get tag() { return 'd-marker' }
 
-  constructor() {
-    super()
-    this._lngLat = [
-      parseInt(this.getAttribute('lng')) || 0, 
-      parseInt(this.getAttribute('lat')) || 0,
-    ]
-  }
+	constructor() {
+		super()
+		this._lngLat = parseCoords(this.getAttribute('c'))
+	}
 
-  async connectedCallback() {
-    await mapboxReady
-    this[mapbox] = new mapboxgl.Marker().setLngLat(this._lngLat)
-    this.lngLat = this._lngLat
-    const map = this.closest(Map.tag)
-    if (map) this[mapbox].addTo(map[mapbox])
-  }
+	async connectedCallback() {
+		this[mapbox] = new mapboxgl.Marker().setLngLat(this._lngLat)
+		this.lngLat = this._lngLat
+		const map = this.closest(Map.tag)
+		if (map) this[mapbox].addTo(map[mapbox])
+	}
 
-  get lngLat() { return this._lngLat }
-  set lngLat([lng, lat]) { set(this, 'lngLat', [lng || 0, lat || 0]) }
-}
-customElements.define(Marker.tag, Marker)
-
-async function set(self, prop, val) {
-  self[`_${prop}`] = val
-  await mapboxReady
-  requestAnimationFrame(() => {
-    prop = prop.charAt(0).toUpperCase() + prop.slice(1)
-    self[mapbox][`set${prop}`](val)
-  })
+	get lngLat() { return this._lngLat }
+	set lngLat([lng, lat]) { setProp(this, 'lngLat', [lng || 0, lat || 0]) }
 }
 
-async function loadMapbox(jsURL, cssURL) {
-  await new Promise((res, rej) => {
-    const script = document.createElement('script')
-    script.onload = res
-    script.src = jsURL
-    document.head.append(script)
-  })
-  mapboxgl.accessToken = mbxToken.content
+function setProp(self, prop, val) {
+	self[`_${prop}`] = val
+	prop = prop.charAt(0).toUpperCase() + prop.slice(1)
+	self[mapbox][`set${prop}`](val)
+}
+
+function parseCoords(str) {
+	const c = `${str}`.split(' ').filter(c => !!c).map(c => +c)
+	return c.length == 2 ? c : [0, 0]
+}
+
+async function loadMapbox(jsURL, token) {
+	await new Promise((res, rej) => {
+		const script = document.createElement('script')
+		script.onload = res
+		script.src = jsURL
+		document.head.append(script)
+	})
+	mapboxgl.accessToken = token
 }
